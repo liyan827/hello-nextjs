ecf1768aefee0ce0d8963f319f238bf5d2997ed7
macondo <liyan.macondo@bytedance.com>
2023-02-02 15:54:48 +0800

    feat: init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  const email = req.body.email;
+  // Then save email to your database, etc...
   res.status(200).json({ name: 'John Doe' })
 }
